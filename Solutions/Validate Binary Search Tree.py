b245a9e4a7ca4330cb83239ba0f022d30f58fb56
selvaganesh19 <selvaganesh.al22@bitsathy.ac.in>
2025-09-24 15:53:20 +0530

    Added solutions for Validate Binary Search Tree
    Co-authored-by: DHARANEEDHARAN V <dharaneedharan.al22@gmail.com>

---
+++ Solutions/Validate Binary Search Tree.py
@@ -0,0 +1,6 @@
+def isValidBST(root) -> bool:
+    def helper(node, low, high):
+        if not node: return True
+        if not (low < node.val < high): return False
+        return helper(node.left, low, node.val) and helper(node.right, node.val, high)
+    return helper(root, float('-inf'), float('inf'))
