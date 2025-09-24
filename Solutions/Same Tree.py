b2083f91b32e6faa1860712e71c7a72bf0448a7c
selvaganesh19 <selvaganesh.al22@bitsathy.ac.in>
2025-09-24 15:49:47 +0530

    Added solutions for Same Tree
    Co-authored-by: DHARANEEDHARAN V <dharaneedharan.al22@gmail.com>

---
+++ Solutions/Same Tree.py
@@ -0,0 +1,5 @@
+def isSameTree(p, q) -> bool:
+    if not p and not q: return True
+    if not p or not q: return False
+    if p.val!=q.val: return False
+    return isSameTree(p.left,q.left) and isSameTree(p.right,q.right)
